7a134b02f407f6c89929fd639a23e5b9de5aa9b7
Kosheira1 <dleonard@student.ethz.ch>
2021-09-25 13:55:53 +0200

    format

--- backend/main.py
+++ backend/main.py
@@ -15,23 +15,23 @@ FLASK_DEBUG = True
 
 @app.route("/")
 def hello_world():
-  return "<p>Hello, World!</p>"
+    return "<p>Hello, World!</p>"
 
 
 @app.route("/users/<string:user_id>/sync")
 def sync_to_google_calendar(user_id: str):
-  return "<p>I synced to google calendar for </p>" + user_id
+    return "<p>I synced to google calendar for </p>" + user_id
 
 
 @app.route("/users/<string:user_id>")
 def get_user(user_id: str):
-  # query database for user_id
-  database_fetch = {
-    "name": "database.user_id.name",
-    "age": 45
-  }
-  return json.dumps(database_fetch)
+    # query database for user_id
+    database_fetch = {
+        "name": "database.user_id.name",
+        "age": 45
+    }
+    return json.dumps(database_fetch)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=PORT, debug=FLASK_DEBUG)
+    app.run(host='0.0.0.0', port=PORT, debug=FLASK_DEBUG)
