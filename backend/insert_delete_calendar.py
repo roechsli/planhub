53a396c37fdb4bb0be22351c95e1cb72aa728e12
Kosheira1 <dleonard@student.ethz.ch>
2021-09-25 13:41:24 +0200

    calendar creation

---
+++ backend/insert_delete_calendar.py
@@ -0,0 +1,32 @@
+from pprint import pprint
+from Google import Create_Service
+
+def calendarservice():
+  CLIENT_SECRET_FILE = 'client_secret_GoogleCloudDemo.json'
+  API_NAME = 'calendar'
+  API_VERSION = 'v3'
+  SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+  service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+  return service
+
+def create_calendar(calendarname, service):
+  response = service.calendarList().list().execute()
+  cal_exists = 0
+  for cal in response.get('items'):
+    if (cal['summary'] == calendarname):
+      cal_exists = 1
+  #pprint(response.get('items'))
+  #pprint(response.get('items')[0])
+  if(cal_exists == 0):
+    print('Creating new calendar')
+    request_body = {
+      'summary': calendarname
+    }
+    # Create calendar
+    response = service.calendars().insert(body=request_body).execute()
+    print(response)
+
+def delete_calendar(calendarId, service):
+  service.calendars().delete(calendarId='gdnu8l2tdps5a1up3j6dbncaoo@group.calendar.google.com').execute()
+
--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,37 @@
 from flask import Flask
 import json
+from insert_delete_calendar import calendarservice, create_calendar, delete_calendar
 
+service = calendarservice()
+create_calendar('PlanHubCalendar 2', service)
 
 app = Flask(__name__)
+print("alls")
 
 # define global constants
-PORT=1337
-FLASK_DEBUG=True
+PORT = 1337
+FLASK_DEBUG = True
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+  return "<p>Hello, World!</p>"
 
 
 @app.route("/users/<string:user_id>/sync")
-def sync_to_google_calendar(user_id:str):
-    return "<p>I synced to google calendar for </p>" + user_id
+def sync_to_google_calendar(user_id: str):
+  return "<p>I synced to google calendar for </p>" + user_id
 
 
 @app.route("/users/<string:user_id>")
-def get_user(user_id:str):
-    # query database for user_id
-    database_fetch = {
-        "name": "database.user_id.name",
-        "age": 45
-    }
-    return json.dumps(database_fetch)
-
+def get_user(user_id: str):
+  # query database for user_id
+  database_fetch = {
+    "name": "database.user_id.name",
+    "age": 45
+  }
+  return json.dumps(database_fetch)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PORT, debug=FLASK_DEBUG)
+  app.run(host='0.0.0.0', port=PORT, debug=FLASK_DEBUG)
